73aad22fd8c66ba06329eaeffea89be313450954
dametriusnorris28 <dametrius.norris28@gmail.com>
2025-03-04 08:51:08 -0600

    Update WN10-CC-000090.ps1

--- STIG/WN10-CC-000090.ps1
+++ STIG/WN10-CC-000090.ps1
@@ -22,7 +22,7 @@
 .USAGE
     Put any usage instructions here.
     Example syntax:
-    PS C:\> .\(STIG-ID-WN10-CC-000090).ps1 
+    PS C:\> .\STIG-ID-WN10-CC-000090.ps1 
 #>
 
 # Define the correct registry path
