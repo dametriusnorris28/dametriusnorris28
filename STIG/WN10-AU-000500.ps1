5565209107279ca778d5651a338ee23f2c5cf0ca
dametriusnorris28 <dametrius.norris28@gmail.com>
2025-03-04 20:53:29 -0600

    Update WN10-AU-000500.ps1

--- STIG/WN10-AU-000500.ps1
+++ STIG/WN10-AU-000500.ps1
@@ -7,7 +7,7 @@
     LinkedIn        : linkedin.com/in/dametrius-norris/
     GitHub          : github.com/dametriusnorris28
     Date Created    : 2025-03-02
-    Last Modified   : 2025-03-02
+    Last Modified   : 2025-03-04
     Version         : 1.0
     CVEs            : N/A
     Plugin IDs      : N/A
